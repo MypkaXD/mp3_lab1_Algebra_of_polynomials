bf7311a450c1a28270af12975caba2ac40f69f00
MypkaXD <thuwashev2016@ayndex.ru>
2023-02-13 13:47:17 +0300

    Test

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -3,10 +3,12 @@
 #include <iostream>
 #include <initializer_list>
 #include <gtest.h>
-
+#include "Forward_List.h"
 
 int main(int argc, char** argv)
 {
 	::testing::InitGoogleTest(&argc, argv);
 	return RUN_ALL_TESTS();
+
+	//test
 }
